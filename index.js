398820fb471d071c657b54b5674800c529e15fcb
mehulkala <mehulkala22@gmail.com>
2025-09-19 18:01:10 +0000

    database integration

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import paymentRoute from "./router/makepayment.js"
+import sql from "./db.js";
 dotenv.config()
 const app= express()
 
@@ -18,17 +19,71 @@ app.use(cors({
 }));
 
 
-
-
 app.use(express.json())
-
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/makepayment",paymentRoute)
 
-app.post("/formsubmit",(req,res)=>{
-    console.log(req.body)
-    res.send({message: "form submitted"})
-})
+app.post("/formsubmit", async (req, res)=>{
+  console.log(req.body);
+  try {
+    const {
+      applicationNumber,
+      name,
+      fatherName,
+      motherName,
+      dob,
+      gender,
+      category,
+      pwd,
+      allIndiaRank,
+      categoryRank,
+      preference1,
+      preference2,
+      preference3,
+      preference4,
+      preference5,
+      state,
+      email,
+      mobileNumber,
+    } = req.body;
+
+    const result = await sql
+      `INSERT INTO admissions (
+        application_number,
+        candidate_name,
+        fathers_name,
+        mothers_name,
+        date_of_birth,
+        gender,
+        category,
+        pwd,
+        all_india_rank,
+        category_rank,
+        preference_1,
+        preference_2,
+        preference_3,
+        preference_4,
+        preference_5,
+        domicile_state,
+        email,
+        mobile_number
+      ) VALUES (
+        ${applicationNumber}, ${name}, ${fatherName}, ${motherName}, ${dob}, ${gender}, ${category}, ${pwd === "Yes"},
+        ${allIndiaRank}, ${categoryRank}, ${preference1}, ${preference2}, ${preference3}, ${preference4}, ${preference5}, ${state}, ${email}, ${mobileNumber}
+      )`
+
+    res.status(201).json({ message: "Form submitted successfully!", data: result[0] });
+  } catch (err) {
+    console.error(err.message);
+    if (err.code === "23505") {
+      // Unique constraint violation (email/application_number)
+      res.status(400).json({ message: "Duplicate entry. Email or Application Number already exists." });
+    } else {
+      res.status(500).json({ message: "Server error. Please try again later." });
+    }
+  }
+});
 
 app.get("/",(req,res)=>{
     res.send("<h1>Welcome to the server</h1>")
