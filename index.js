f4ee80160601f8d5b5744b4063136e8288071980
silent-hyena <ayushpanwar00007@gmail.com>
2025-09-23 00:45:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-// import sql from "./db.js";
+import sql from "./db.js";
 
 
 import paymentRoute from "./router/makepayment.js"
@@ -13,8 +13,8 @@ import admissionRoute from "./router/admission.js"
 
 
 dotenv.config()
-const app= express()
-app.set('trust proxy', 1); 
+const app = express()
+app.set('trust proxy', 1);
 
 
 
@@ -23,14 +23,14 @@ app.use(cookieParser())
 
 
 const allowedOrigins = [
-  "http://localhost:5173",        
-  "https://collegeerp442.vercel.app"  
+  "http://localhost:5173",
+  "https://collegeerp442.vercel.app"
 ];
 
 app.use(cors({
   origin: allowedOrigins,
   credentials: true,
-  optionsSuccessStatus: 200  
+  optionsSuccessStatus: 200
 }));
 
 
@@ -41,20 +41,39 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 
-app.use("/makepayment",paymentRoute)
-app.use("/emailservice",EmailRoute)
-app.use("/staff",staffRoute)
-app.use("/student",studentRoute)
-app.use("/admission",admissionRoute)
+app.use("/makepayment", paymentRoute)
+app.use("/emailservice", EmailRoute)
+app.use("/staff", staffRoute)
+app.use("/student", studentRoute)
+app.use("/admission", admissionRoute)
 
 
+app.get("/tables", async (req, res) => {
+  try {
+    const columns = await sql`
+     SELECT 
+    table_schema,
+    table_name,
+    json_agg(column_name ORDER BY ordinal_position) AS columns
+FROM information_schema.columns
+WHERE table_schema = 'public'
+GROUP BY table_schema, table_name
+ORDER BY table_schema, table_name;
 
-
-
-
-app.get("/",(req,res)=>{
-    res.send("<h1>Welcome to the server</h1>")
+    `;
+    res.json(columns);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching columns");
+  }
 })
-app.listen(process.env.PORT,()=>{
-    console.log("server running")
+
+
+
+
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to the server</h1>")
+})
+app.listen(process.env.PORT, () => {
+  console.log("server running")
 })
